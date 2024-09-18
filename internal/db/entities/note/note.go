2ef3a8318343a29d8e9d806ca476cb062ae3366b
Emily <emily@anaboth.com>
2024-09-18 13:55:41 -0300

    feat: add note model

---
+++ internal/db/entities/note/note.go
@@ -0,0 +1,20 @@
+package note
+
+import (
+	"friendly-backend/internal/db/entities/child"
+	"friendly-backend/internal/db/entities/log"
+	"friendly-backend/internal/db/entities/person"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Participation struct {
+	ID          uuid.UUID     `json:"id"`
+	IDPerson    person.Person `json:"id_person"`
+	IDChild     child.Child   `json:"id_child"`
+	Date        time.Time     `json:"date"`
+	Description string        `json:"description"`
+	CreateLog   log.Log       `json:"create_log"`
+	UpdateLog   log.Log       `json:"update_log"`
+}
