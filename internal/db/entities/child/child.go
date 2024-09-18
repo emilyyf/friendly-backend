f8b8aae9311ece73a9a99aaf9632fe7d4b53d8c6
Emily <emily@anaboth.com>
2024-09-18 13:52:11 -0300

    fix: some fields had the incorrect type

--- internal/db/entities/child/child.go
+++ internal/db/entities/child/child.go
@@ -15,29 +15,29 @@ type Child struct {
 	IDMother              person.Person       `json:"id_mother"`
 	Name                  string              `json:"name"`
 	Birth                 time.Time           `json:"birth"`
-	Age                   int64               `json:"age"`
+	Age                   string              `json:"age"`
 	Local                 string              `json:"local"`
 	Race                  string              `json:"race"`
 	AliveBirthCertificate string              `json:"alive_birth_certificate"`
 	BirthCertificate      string              `json:"birth_certificate"`
 	RG                    string              `json:"rg"`
 	CPF                   string              `json:"cpf"`
-	SUSCard               int64               `json:"sus_card"`
+	SUSCard               string              `json:"sus_card"`
 	BloodType             string              `json:"blood_type"`
-	WeightAtBirth         int64               `json:"weight_at_birth"`
-	HeightAtBirth         int64               `json:"height_at_Birth"`
-	FirstApgar            int64               `json:"first_apgar"`
-	FifthApgar            int64               `json:"fifth_apgar"`
-	NeonatalHeelPrick     int64               `json:"neonatal_heel_prick"`
-	HearTest              int64               `json:"hear_test"`
-	HearingTriage         int64               `json:"hearing_test"`
-	EyeTest               int64               `json:"eye_test"`
-	OD                    int64               `json:"od"`
-	OE                    int64               `json:"oe"`
-	PregnancyTime         int64               `jsno:"pregnancy_time"`
-	Login                 int64               `json:"login"`
-	MSD                   int64               `json:"msd"`
-	MMII                  int64               `json:"mmii"`
+	WeightAtBirth         string              `json:"weight_at_birth"`
+	HeightAtBirth         string              `json:"height_at_Birth"`
+	FirstApgar            string              `json:"first_apgar"`
+	FifthApgar            string              `json:"fifth_apgar"`
+	NeonatalHeelPrick     time.Time           `json:"neonatal_heel_prick"`
+	HearTest              time.Time           `json:"hear_test"`
+	HearingTriage         time.Time           `json:"hearing_test"`
+	EyeTest               string              `json:"eye_test"`
+	OD                    string              `json:"od"`
+	OE                    string              `json:"oe"`
+	PregnancyTime         string              `json:"pregnancy_time"`
+	Login                 string              `json:"login"`
+	MSD                   string              `json:"msd"`
+	MMII                  string              `json:"mmii"`
 	CreateLog             log.Log             `json:"create_log"`
 	UpdateLog             log.Log             `json:"update_log"`
 }
