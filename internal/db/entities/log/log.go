4a015d32f9c0f04b0aa53c722f2653c0a1863b20
Dnreikronos <dnreikronos2@gmail.com>
2024-09-21 15:19:16 -0300

    fix: fix imports in log entity

--- internal/db/entities/log/log.go
+++ internal/db/entities/log/log.go
@@ -1,18 +1,18 @@
 package log
 
 import (
-	"friendly-backend/internal/models"
+	"friendly-backend/internal/db/entities/user"
 	"time"
 
 	"github.com/google/uuid"
 )
 
 type Log struct {
-	ID          uuid.UUID           `json:"id"`
-	UserID      models.UserResponse `json:"user_id"`
-	Table       string              `json:"table"`
-	Date        time.Time           `json:"date"`
-	Description string              `json:"description"`
-	Action      string              `json:"action"`
-	RowID       uuid.UUID           `json:"row_id"`
+	ID          uuid.UUID         `json:"id"`
+	UserID      user.UserResponse `json:"user_id"`
+	Table       string            `json:"table"`
+	Date        time.Time         `json:"date"`
+	Description string            `json:"description"`
+	Action      string            `json:"action"`
+	RowID       uuid.UUID         `json:"row_id"`
 }
