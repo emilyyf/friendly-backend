2c21c9543ca054ddb0a0d51d0da6ce1e72d5a563
Dnreikronos <dnreikronos2@gmail.com>
2024-10-03 21:28:13 -0300

    feat: create jwt for user authentication

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	v1 "friendly-backend/internal/api/v1/user"
 	conn "friendly-backend/internal/db/connection"
+	"friendly-backend/internal/handlers"
 	"net/http"
 	"os"
 
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
@@ -15,16 +16,28 @@ func main() {
 		panic(err)
 	}
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("GET /v1/user", v1.GetUserHandler)
-	mux.HandleFunc("POST /v1/user", v1.CreateUserHandler)
-
 	db, err := conn.OpenConnection()
 	if err != nil {
 		panic(err)
 	}
+	
+  conn.RunMigrations(db)
 
-	conn.RunMigrations(db)
+	r := gin.Default()
 
-	http.ListenAndServe(":"+os.Getenv("PORT"), mux)
+	r.Use(func(c *gin.Context) {
+		c.Set("db", db)
+		c.Next()
+	})
+
+	r.POST("/register", handlers.CreateUserHandler)
+	r.POST("/login", handlers.LoginHandler)
+
+	authorized := r.Group("/", handlers.AuthMiddleware())
+	{
+		authorized.GET("/profile", handlers.ProfileHandler)
+	}
+
+	http.ListenAndServe(":"+os.Getenv("PORT"), r)
 }
+
---
+++ internal/handlers/user_handler.go
@@ -0,0 +1,134 @@
+package handlers
+
+import (
+	"friendly-backend/internal/db/entities/user"
+	"net/http"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
+)
+
+var jwtSecret = []byte("your_secret_key")
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+func VerifyPassword(hashedPassword, password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+}
+
+func GenerateToken(user user.User) (string, error) {
+	expirationTime := time.Now().Add(24 * time.Hour)
+	claims := &jwt.StandardClaims{
+		Subject:   user.ID.String(),
+		ExpiresAt: expirationTime.Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(jwtSecret)
+}
+
+func CreateUserHandler(c *gin.Context) {
+	var input user.SignInInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	hashedPassword, err := HashPassword(input.Password)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to hash password"})
+		return
+	}
+
+	newUser := user.User{
+		Email:    input.Email,
+		Password: hashedPassword,
+		Verified: true,
+	}
+
+	db := c.MustGet("db").(*gorm.DB)
+	if err := db.Create(&newUser).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create user"})
+		return
+	}
+
+	c.JSON(http.StatusCreated, user.FilteredResponse(newUser))
+}
+
+func LoginHandler(c *gin.Context) {
+	var input user.SignInInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	var existingUser user.User
+	db := c.MustGet("db").(*gorm.DB)
+	if err := db.Where("email = ?", input.Email).First(&existingUser).Error; err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid credentials"})
+		return
+	}
+
+	if err := VerifyPassword(existingUser.Password, input.Password); err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid credentials"})
+		return
+	}
+
+	token, err := GenerateToken(existingUser)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Could not generate token"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"token": token})
+}
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		tokenString := c.Request.Header.Get("Authorization")
+		if tokenString == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "No token provided"})
+			c.Abort()
+			return
+		}
+
+		claims := &jwt.StandardClaims{}
+		token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+			return jwtSecret, nil
+		})
+
+		if err != nil || !token.Valid {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+			c.Abort()
+			return
+		}
+
+		c.Set("userID", claims.Subject)
+		c.Next()
+	}
+}
+
+
+func ProfileHandler(c *gin.Context) {
+	userID := c.MustGet("userID").(string)
+
+	var u user.User
+	db := c.MustGet("db").(*gorm.DB)
+	if err := db.First(&u, "id = ?", userID).Error; err != nil {
+		if err == gorm.ErrRecordNotFound {
+			c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
+		} else {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal server error"})
+		}
+		return
+	}
+
+	c.JSON(http.StatusOK, user.FilteredResponse(u))
+}
+
